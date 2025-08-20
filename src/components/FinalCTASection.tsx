f18b6aabe739e2e7307bb8ddd99255bf24394701
George Borunov <george@borunov.com>
2025-08-19 23:10:48 -0400

    fix gtag conversion

--- src/components/FinalCTASection.tsx
+++ src/components/FinalCTASection.tsx
@@ -27,11 +27,11 @@ const FinalCTASection = () => {
       if (error) {
         throw error;
       }
-
+      
       // Track gtag conversion when email is successfully submitted
       if (typeof window !== 'undefined' && window.gtag) {
         window.gtag('event', 'conversion', {
-          'send_to': 'AW-17434943664/lead',
+          'send_to': 'AW-17434943664/J87fCIO25YkbELDB0PlA',
           'value': 1.0,
           'currency': 'USD'
         });
